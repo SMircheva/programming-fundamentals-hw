f8b3539d0eeeacbb711c1b8219a61aaf255ad933
Silviya Mircheva <mircheva.silviya@gmail.com>
2017-12-29 12:43:13 +0200

    lab from data types and variables part 1

---
+++ Data-Type-And-Variables-Lecture/CenturiesToMinutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var centuries = int.Parse(Console.ReadLine());
+            var years = centuries * 100;
+            var days = (int)(years * 365.2422);
+            var hours = days * 24;
+            var minutes = hours * 60;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
---
+++ Data-Type-And-Variables-Lecture/ExactSum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sum = 0M;
+            for (int i = 0; i < n; i++)
+            {
+                decimal currentNum = decimal.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
