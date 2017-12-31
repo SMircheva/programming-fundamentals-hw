1282542ff28080c8a154b0a298d3959fb65fd8af
Silviya Mircheva <mircheva.silviya@gmail.com>
2017-12-31 14:25:43 +0200

    Data types and variables - exercise

---
+++ Data-Type-And-Variables-Exercise/CharsAndStrings/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            char line2 = char.Parse(Console.ReadLine());
+            char line3 = char.Parse(Console.ReadLine());
+            char line4 = char.Parse(Console.ReadLine());
+            string line5 = Console.ReadLine();
+
+            Console.WriteLine(line1);
+            Console.WriteLine(line2);
+            Console.WriteLine(line3);
+            Console.WriteLine(line4);
+            Console.WriteLine(line5);
+
+
+        }
+    }
+}
