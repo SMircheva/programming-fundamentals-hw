e688a6fdb0afa8ad25538d7a548d02a4bae5c478
Silviya Mircheva <mircheva.silviya@gmail.com>
2017-12-31 18:00:48 +0200

    Data types and variables in progress

---
+++ Data-Type-And-Variables-Exercise/ASCIITable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte firstNum = byte.Parse(Console.ReadLine());
+            byte secondNum = byte.Parse(Console.ReadLine());
+
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.Write((char)i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Data-Type-And-Variables-Exercise/IntToHexBinary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToHexBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            var hexNumber = Convert.ToString(input, 16);
+            hexNumber = hexNumber.ToUpper();
+            var binNumber = Convert.ToString(input, 2);
+            binNumber = binNumber.ToUpper();
+            Console.WriteLine(hexNumber);
+            Console.WriteLine(binNumber);
+            
+        }
+    }
+}
