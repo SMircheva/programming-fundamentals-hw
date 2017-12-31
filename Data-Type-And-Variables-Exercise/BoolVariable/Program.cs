91998e5f1752eed81bab5d65caeb2d43ee06ed94
Silviya Mircheva <mircheva.silviya@gmail.com>
2017-12-31 15:21:21 +0200

    data types and variables additions

---
+++ Data-Type-And-Variables-Exercise/BoolVariable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoolVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isTrue = bool.Parse(Console.ReadLine());
+
+            if (isTrue)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
---
+++ Data-Type-And-Variables-Exercise/HexadecimalConvert/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HexadecimalConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var hexNumber = Console.ReadLine();
+            var newHexNumber = hexNumber.Substring(2);
+            var baseTenNumber = Convert.ToInt32(newHexNumber, 16);
+            Console.WriteLine(baseTenNumber);
+        }
+    }
+}
