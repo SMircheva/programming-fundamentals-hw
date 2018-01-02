0f65001de94a378ea395b73fbe1b895e5daa9aa0
Silviya Mircheva <mircheva.silviya@gmail.com>
2018-01-02 23:16:21 +0200

    Methods and debugging lecture

---
+++ Methods, Debugging and Troubleshootinh/DrawFilledSquare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawFilledSquare
+{
+    class Program
+    {
+        static void DrawLine(int num)
+        {
+            Console.WriteLine(new string('-', 2 * num));            
+        }
+        static void DrawSlashes(int num)
+        {
+            Console.Write("-");
+            for (int i = 0; i < num - 1; i++)
+            {
+                Console.Write("\\/");
+            }
+            Console.WriteLine("-");
+        }
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            DrawLine(n);
+            for (int i = 0; i < n-2; i++)
+            {
+                DrawSlashes(n);
+            }
+            DrawLine(n);
+
+        }
+    }
+}
---
+++ Methods, Debugging and Troubleshootinh/SignOfInt/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfInt
+{
+    class Program
+    {
+        static void CheckSign(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine($"The number {number} is negative.");
+            }
+            else if (number == 0)
+            {
+                Console.WriteLine("The number 0 is zero.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is positive.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            CheckSign(n);
+        }
+    }
+}
