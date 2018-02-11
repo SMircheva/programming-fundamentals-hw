f3dbd4345a7e37a2098a6bdd95b1e36ceaa81566
Silviya Mircheva <mircheva.silviya@gmail.com>
2018-02-11 23:39:03 +0200

    lists array

---
+++ Lists - Lecture/03.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+            bool noSames = false;
+            do
+            {
+                int startCount = input.Count;
+
+                for (int i = 1; i < input.Count; i++)
+                {
+                    if (input[i] == input[i - 1])
+                    {
+                        input[i - 1] *= 2;
+                        input.RemoveAt(i);
+                    }
+                    
+                }
+                if (startCount == input.Count)
+                {
+                    noSames = true;
+                }
+            }
+            while (!noSames);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+    }
+}
---
+++ Lists - Lecture/04.SplitByCasing/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SplitByCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> input = Console.ReadLine()
+                .Split(",;:.!()\"'\\/[] ".ToCharArray
+                (),StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> allUpper = new List<string>();
+            List<string> allLower = new List<string>();
+            List<string> mixed = new List<string>();
+
+            foreach (var word in input)
+            {
+                if (IsUpperWord(word))
+                {
+                    allUpper.Add(word);
+                }
+                else if (IsLowerWord(word))
+                {
+                    allLower.Add(word);
+                }
+                else
+                {
+                    mixed.Add(word);
+                }
+            }
+
+            Console.WriteLine("Lower-case: " + string.Join(", ", allLower));
+            Console.WriteLine("Mixed-case: " + string.Join(", ", mixed));
+            Console.WriteLine("Upper-case: " + string.Join(", ", allUpper));
+
+        }
+
+        static bool IsUpperWord (string word)
+        {
+            foreach (var symbol in word)
+            {
+                if (symbol < 'A' || symbol > 'Z')
+                {
+                    return false;
+                }
+                          
+            }
+            return true;
+        }
+
+        static bool IsLowerWord(string word)
+        {
+            foreach (var symbol in word)
+            {
+                if (symbol < 'a' || symbol > 'z')
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+    }
+}
