03cdb824de67398db1fac1d2559a158e5af6a83b
Silviya Mircheva <mircheva.silviya@gmail.com>
2018-06-01 10:27:49 +0300

    adding exam problems

---
+++ Exam/p03/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace p03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"@([a-zA-Z]+)[^@\-!>:]*:([0-9.]+)[^@\-!>:]*!(A|D)![^@\-!>:]*->([0-9.]+)";
+            List<string> attacked = new List<string>();
+            List<string> destroyed = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                int key = CountStar(input);
+                string decryptedMessage = Dectypt(input, key);
+                
+                if (!Regex.IsMatch(decryptedMessage, pattern))
+                {
+                    continue;
+                }
+                
+                Match match = Regex.Match(decryptedMessage, pattern);
+                
+
+                if (match.Groups[3].ToString() == "A")
+                {
+                    attacked.Add(match.Groups[1].ToString());
+                }
+                else // check if the other one is indeed A?
+                {
+                    destroyed.Add(match.Groups[1].ToString());
+                }
+                
+            }
+
+            Console.WriteLine($"Attacked planets: {attacked.Count}");
+            foreach (var planet in attacked.OrderBy(p => p))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+
+            Console.WriteLine($"Destroyed planets: {destroyed.Count}");
+            foreach (var planet in destroyed.OrderBy(p => p))
+            {
+                Console.WriteLine($"-> {planet}");
+            }
+        }
+
+        static int CountStar (string input)
+        {
+            int counter = 0;
+            input = input.ToLower();
+            foreach (var c in input)
+            {
+                if (c == 's' || c == 't' || c == 'a' || c == 'r')
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static string Dectypt (string input, int key)
+        {
+            StringBuilder decryptedMessage = new StringBuilder();
+
+            foreach (var letter in input)
+            {
+                char letterToAdd = (char)(letter - key);
+                decryptedMessage.Append(letterToAdd);
+            }
+
+            return decryptedMessage.ToString();
+        }
+    }
+}
---
+++ Exam/p04/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> sides = new Dictionary<string, List<string>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Lumpawaroo")
+                {
+                    break;
+                }
+                string[] request = input.Split("|->".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                                
+                if (input.Contains("|")) //this is an add request
+                {
+                    string forceSide = request[0].Trim();
+                    string forceUser = request[1].Trim();
+                    if (!sides.ContainsKey(forceSide))
+                    {
+                        // this force side doesn't exist - add it
+                        List<string> listToAdd = new List<string>();
+                        listToAdd.Add(forceUser);
+                        sides.Add(forceSide, listToAdd);
+                    }
+                    else if (!sides.Values.Any(x => x.Contains(forceUser)))
+                    {
+                        // this user is not on any side - add it to where he wants
+                        sides[forceSide].Add(forceUser);
+                    }
+                    else
+                    {
+                        //he esists - don't do anything (probably - possible issue)
+                        
+                    }
+                }
+                else if (input.Contains("->")) // this is a move request
+                {
+                    string forceSide = request[1].Trim();
+                    string forceUser = request[0].Trim();
+                    if (!sides.ContainsKey(forceSide))
+                    {
+                        // remove him from wherever he is and create new force                        
+                        foreach (var kvp in sides)
+                        {
+                            if (kvp.Value.Contains(forceUser))
+                            {
+                                kvp.Value.Remove(forceUser);
+                            }
+                        }
+                        List<string> listToAdd = new List<string>();
+                        listToAdd.Add(forceUser);
+                        sides.Add(forceSide, listToAdd);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+
+                    }
+                   /* else if (!sides.Values.Any(x => x.Contains(forceUser)))
+                    {
+                        // this user is not currently on any side - add him to whereever he wants
+                        sides[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+                    }*/
+                    else if (sides[forceSide].Contains(forceUser))
+                    {
+                        //he's already where he wants - do nothing                      
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+
+                    }
+                    else
+                    {
+                        //remove user from the side where he is and add to the one he requests
+                        foreach (var kvp in sides)
+                        {
+                            if (kvp.Value.Contains(forceUser))
+                            {
+                                kvp.Value.Remove(forceUser);
+                            }
+                        }
+                        sides[forceSide].Add(forceUser);
+                        Console.WriteLine($"{forceUser} joins the {forceSide} side!");
+
+                    }
+                }
+            }
+
+            foreach (var side in sides.OrderByDescending(x => x.Value.Count).ThenBy(k => k.Key))
+            {
+                if (side.Value.Count == 0)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    Console.WriteLine($"Side: {side.Key}, Members: {side.Value.Count}");
+                    foreach (var user in side.Value.OrderBy(u => u))
+                    {
+                        Console.WriteLine($"! {user}");
+                    }
+                }
+            }
+        }
+    }
+}
